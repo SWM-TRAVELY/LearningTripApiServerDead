1cae333264acd180bf0343338ae1e08d174c5cf5
sieun <supiya0908@naver.com>
2022-08-06 07:02:05 +0900

    Style: Modify entity to domain

--- src/main/java/app/learningtrip/apiserver/place/repository/PlaceTempRepository.java
+++ src/main/java/app/learningtrip/apiserver/place/repository/PlaceTempRepository.java
@@ -1,6 +1,6 @@
 package app.learningtrip.apiserver.place.repository;
 
-import app.learningtrip.apiserver.place.entity.PlaceTempEntity;
+import app.learningtrip.apiserver.place.domain.PlaceTempEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
